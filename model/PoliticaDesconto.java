c4e02fe0ac9d1bbfd41fc82adcf0562b93645bb8
Renata <renatafava.aluno@unipampa.edu.br>
2026-05-07 15:05:44 -0300

    feat: códigos corrigidos

--- model/PoliticaDesconto.java
+++ model/PoliticaDesconto.java
@@ -1,1 +1,6 @@
+//Define um contrato para o cálculo de preços, permitindo flexibilidade para novos perfis de clientes no futuro.
+package model;
 
+public interface PoliticaDesconto{
+    double calcularPrecoFinal(double precoBase);
+}
--- service/DescontoEstudante.java
+++ service/DescontoEstudante.java
@@ -1,1 +1,18 @@
+//Implementa a interface PoliticaDesconto para aplicar descontos percentuais
 
+package service;
+
+import model.PoliticaDesconto;
+
+public class DescontoEstudante implements PoliticaDesconto{
+    private double percentual;
+
+    public DescontoEstudante(double percentual){
+        this.percentual = percentual;
+    }
+
+    @Override
+    public double calcularPrecoFinal(double precoBase){
+        return precoBase * (1 - (percentual / 100));
+    }
+}
